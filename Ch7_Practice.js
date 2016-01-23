36bd041303ac9e48804b41909105ffca21713f34
Sean Dinan <seancdinan@gmail.com>
2016-01-22 19:22:33 -0500

    I think I've got enough of the concepts of this project chapter. The rest is mostly repetative so it's probably a better use of time to move on to Ch 8.

--- Ch7_Practice.js
+++ Ch7_Practice.js
@@ -148,6 +148,120 @@ function Wall() {}
 var world = new World(plan, {'#': Wall, 'o': BouncingCritter});
 console.log(world.toString());
 
+// Let's create a forEach prototype function that supports
+// a context parameter. It calls a given function for each
+// element in the grid that isn't null or undefined.
+Grid.prototype.forEach = function(f, context) {
+	for (var y = 0; y < this.height; y++) {
+		for (var x = 0; x < this.width; x++) {
+			var value = this.space[x + y * this.width];
+			if (value != null)
+				f.call(context, value, new Vector(x, y));
+		}
+	}
+};
+
+// Next, we write a 'turn' method for the world object
+// that gives the critters a chance to act. It'll go over
+// the grid using forEach and look for objects with an 'act'
+// method to get an action object and carry out the action.
+// To avoid double-moving creatures, we'll need an array
+// of critters that have already moved.
+World.prototype.turn = function() {
+	var acted = [];
+	this.grid.forEach(function(critter, vector) {
+		if (critter.act && acted.indexOf(critter) == -1) {
+			acted.push(critter);
+			this.letAct(critter, vector);
+		}
+	}, this);
+};
+
+// The letAct method contains the actual logic that allows
+// the critters to move.
+World.prototype.letAct = function(critter, vector) {
+	var action = critter.act(new View(this, vector));
+	if (action && action.type == 'move') {
+		var dest = this.checkDestination(action, vector);
+		if (dest && this.grid.get(dest) == null) {
+			this.grid.set(vector, null);
+			this.grid.set(dest, critter);
+		}
+	}
+};
+
+World.prototype.checkDestination = function(action, vector) {
+	if (directions.hasOwnProperty(action.direction)) {
+		var dest = vector.plus(directions[action.direction]);
+		if (this.grid.isInside(dest))
+			return dest;
+	}
+}
+
+// The only part we're still missing is the 'View' type.
+function View(world, vector) {
+	this.world = world;
+	this.vector = vector;
+}
+View.prototype.look = function(dir) {
+	var target = this.vector.plus(directions[dir]);
+	if (this.world.grid.isInside(target))
+		return charFromElement(this.world.grid.get(target));
+	else
+		return '#';
+};
+View.prototype.findAll = function(ch) {
+	var found = [];
+	for (var dir in directions)
+		if (this.look(dir) == ch)
+			found.push(dir);
+		return found;
+};
+View.prototype.find = function(ch) {
+	var found = this.findAll(ch);
+	if (found.length == 0) return null;
+	return randomElement(found);
+}
+
+// Let's now add some functions to make a critter
+// that can walk alongside a wall
+function dirPlus(dir, n) {
+	var index = directionNames.indexOf(dir);
+	return directionNames[(index + n + 8) % 8];
+}
+
+function WallFollower() {
+	this.dir = 's';
+}
+
+// This act method scans the surroundings, starting
+// at the left and going clockwise in its search for an
+// empty space.
+WallFollower.prototype.act = function(view) {
+	var start = this.dir;
+	if (view.look(dirPlus(this.dir, -3)) != ' ')
+		start = this.dir = dirPlus(this.dir, -2);
+	while (view.look(this.dir) != ' ') {
+		this.dir = dirPlus(this.dir, 1);
+		if (this.dir == start) break;
+	}
+	return {type: "move", direction: this.dir};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
