1f53510c2e412e6ec2e79c206713a8ed3d3cc344
Sean Dinan <seancdinan@gmail.com>
2016-01-27 11:50:27 -0500

    Done reading Chapter 13, onto the problems next

---
+++ Ch13_Practice.js
@@ -0,0 +1,17 @@
+//*************************************************
+//***************** Finding Elements   ************
+//*************************************************
+
+var link = document.body.getElementsByTagName('a')[0];
+console.log(link.href);
+
+// You can use an html id attribute to reference something
+// specifically via JS.
+var ostrich = document.getElementById('gertrude');
+console.log(ostrich.src);
+// getElementByClass is also a thing.
+
+//*************************************************
+//********** Changing the Document   **************
+//*************************************************
+
